7333f9e65cb6344549e5bb8d8b015089de921653
Rodrigo Marzullo <rodrigo.marzullo@gmail.com>
2018-10-03 22:58:58 -0300

    Add file structure

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  createStackNavigator,
+} from 'react-navigation';
+
+
+
 
 export default class App extends React.Component {
   render() {
